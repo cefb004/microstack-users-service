c4409439f4de63b6125ffd09fd61ddca3327af71
Carlos Brandao <cefb004@gmail.com>
2025-12-02 02:29:30 -0300

    Spring Boot Project Initialized

---
+++ src/main/java/com/microstack/microstack_users/MicrostackUsersApplication.java
@@ -0,0 +1,13 @@
+package com.microstack.microstack_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrostackUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrostackUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microstack/microstack_users/MicrostackUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microstack.microstack_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrostackUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
